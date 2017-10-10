3e5d37c8970453c0442f4479a6a6aa69280a9545
Massimo Santoli <massimo@gimasi.ch>
2017-10-10 09:51:55 +0200

    no message

--- gmx_catm1.py
+++ gmx_catm1.py
@@ -158,7 +158,7 @@ while join_status == 0:
 
     # check response
     index = response.find(":")
-    print "INDEX:"+index
+    print "INDEX:"+str(index)
     if (index!=-1):
         data = response[index:]   
         print "DEBUG2:"+data
